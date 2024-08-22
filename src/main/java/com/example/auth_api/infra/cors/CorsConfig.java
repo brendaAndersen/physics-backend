8cc12c20dbf13a81109de0910e5faf0d405f3310
brendaAndersen <52722720+brendaAndersen@users.noreply.github.com>
2024-08-22 12:42:14 +0000

    updated cors

--- src/main/java/com/example/auth_api/infra/cors/CorsConfig.java
+++ src/main/java/com/example/auth_api/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://physics-backend.onrender.com")
+                .allowedOrigins("http://localhost:5173", "https://physics-backend.onrender.com", "physics-bay-seven.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
